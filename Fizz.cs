8d27d23625b1d3f919fb896bd3e2f19ce26dbc51
User <User@HP_NOTEBOOK>
2017-02-01 10:36:48 -0500

    importing to github

---
+++ Fizz.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Fizz
+{ 
+  
+  
+	public Fizz(ref int min, int max)
+	{
+        this.min = min;
+        this.max = max;
+
+        if(min > max)
+        {
+            int temp = min;
+            min = max;
+            max = temp;
+        }
+        results = new string[max];
+        calculate();
+        min = 4;
+	}
+
+    public string[] results;
+    public int min;
+    public int max;
+
+    public void calculate()
+    {
+        int counter = 0;
+        for(int i = min; i  <= max ; i++)
+        {
+            
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                results[counter] = "FizzBuzz";
+            }
+            else if(i % 3 == 0)
+            {
+                results[counter] = "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                results[counter] = "Buzz";
+            }
+            counter = counter + 1;
+        }
+    }
+
+    public override String ToString()
+    {
+        String result = "";
+
+        for(int j =0; j < results.Length; j ++)
+        {
+            if(results[j]!= null)
+            {
+              //  Console.WriteLine(results[j]);
+                result += results[j] + "\n";
+            }
+            
+        }
+        return result;
+    }
+     
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using FizzBuzz.F
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+            int j = 1;
+            Fizz Fizz = new Fizz(ref j,100);
+            Console.WriteLine("YAY");
+
+            //String d = "qwdsad";
+            Console.WriteLine( Fizz.ToString());
+            Console.ReadLine();
+            Console.WriteLine(j);
+            Console.ReadLine();
+        }
+    }
+}
